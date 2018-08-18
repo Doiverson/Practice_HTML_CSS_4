1c6841351d8456c471eb70968ed23d9b8f13094b
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-08-18 15:44:09 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,21 @@ var plumber = require('gulp-plumber');
 var bs = require('browser-sync').create();
 
 // browser-sync
-gulp.task('bs', function(){
-    var bsOptions = {}
-    bsOptions.files = ['./**/index.html', './css/**/*.css'];
-    bsOptions.port  = 3000;
-    bs.init(bsOptions);
+// gulp.task('bs', function(){
+//     var bsOptions = {}
+//     bsOptions.files = ['./**/index.html', './css/**/*.css'];
+//     bsOptions.port  = 3000;
+//     bs.init(bsOptions);
+// });
+
+// タスク名と変数名は別々にしといた方が見やすいよ :)
+gulp.task("browserSync", function () {
+    bs.init({
+        server: {
+            baseDir: "./", // ルートとなるディレクトリを指定
+        },
+        port: 8080 // Portはこのオプションで変えれる
+    });
 });
 
 // Sassコンパイルタスク
@@ -16,16 +26,28 @@ gulp.task('sass', function(){
     gulp.src('./scss/**/*.scss')
         .pipe(plumber()) // ←ここが追加
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./css/'))
+        .pipe(bs.stream()) // Sassのコンパル後、BrowserSyncのリロードを行う
+});
+
+gulp.task('html', function() {
+    gulp.src(['**/*.html'])
+        .pipe(bs.stream())
 });
 
 // watchタスク(**/*.scss変更時に実行するタスク)
-gulp.task('sass-watch', ['sass'], function(){
-    var watcher = gulp.watch('./scss/**/*.scss', ['sass']);
-    watcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-});
+// gulp.task('sass-watch', ['sass'], function(){
+//     var watcher = gulp.watch('./scss/**/*.scss', ['sass']);
+//     watcher.on('change', function(event) {
+//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//     });
+// });
 
 // gulpのデフォルト動作としてsass-watchを実行
-gulp.task('default', ['bs','sass-watch']);
+// gulp.task('default', ['bs','sass-watch']);
+
+// gulpのデフォルトはwatchタスクを一緒に書いた方が楽かな
+gulp.task('default', ['browserSync'], function () {
+    gulp.watch('scss/**/*.scss',['sass']);
+    gulp.watch('**/*.html', ['html']);
+});
